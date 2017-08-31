207978521146416bca13c92c2964be3161e7263b
Marcelo Marra <marcelo.marra@gmail.com>
2017-08-30 22:59:45 -0300

    [#5] Calling NHTSA api.

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,16 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\NHTSA\GetSafetyRankings;
+
 class VehicleController extends Controller
 {
     /*
      * Search for a vehicle
+     *
+     * @param $model_year
+     * @param $manufacturer
+     * @param $model
+     * @return \Illuminate\Http\JsonResponse
      */
     public function search($model_year, $manufacturer, $model)
     {
-        return response()->json([
-            'Count' => 0,
-            'Results' => []
-        ], 200);
+        return $this->performSearch($model_year, $manufacturer, $model);
+    }
+
+    /**
+     * @param $model_year
+     * @param $manufacturer
+     * @param $model
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function performSearch($model_year, $manufacturer, $model)
+    {
+        $response = $this->dispatch(new GetSafetyRankings($model_year, $manufacturer, $model));
+        $collection = collect($response['Results']);
+        $transformed = $collection->reduce(function ($lookup, $vehicle) {
+            $transformedVehicle = [];
+            $transformedVehicle['Description'] = $vehicle['VehicleDescription'];
+            $transformedVehicle['VehicleId'] = $vehicle['VehicleId'];
+            $lookup[] = $transformedVehicle;
+            return $lookup;
+        }, []);
+        $response['Results'] = $transformed;
+        return response()->json($response, 200);
     }
 }
---
+++ app/Jobs/NHTSA/GetSafetyRankings.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Jobs\NHTSA;
+
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class GetSafetyRankings
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var
+     */
+    private $modelYear;
+    /**
+     * @var
+     */
+    private $make;
+    /**
+     * @var
+     */
+    private $model;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $modelYear
+     * @param $make
+     * @param $model
+     */
+    public function __construct($modelYear, $make, $model)
+    {
+        $this->modelYear = $modelYear;
+        $this->make = $make;
+        $this->model = $model;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return array
+     */
+    public function handle()
+    {
+        $client = new Client();
+        $url = env('NHTSA_API_URL') . "modelyear/$this->modelYear/make/$this->make/model/$this->model?format=json";
+        $response = $client->request('GET', $url);
+        if ($response->getStatusCode() === 200) {
+            return json_decode($response->getBody(), true);
+        } else {
+            return [
+                'Count' => 0,
+                'Results' => []
+            ];
+        }
+    }
+}
